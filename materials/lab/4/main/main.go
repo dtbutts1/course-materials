1e301c220306353b443b3f3d8177c1395782dc07
dbutts1 <dbutts1@uwyo.edu>
2022-03-08 17:57:16 -0500

    Completed Lab 4, did option 1

--- materials/lab/4/main/main.go
+++ materials/lab/4/main/main.go
@@ -10,6 +10,7 @@ import (
 
 func main() {
 	wyoassign.InitAssignments()
+	wyoassign.InitClasses()
 	log.Println("starting API server")
 	//create a new router
 	router := mux.NewRouter()
@@ -20,11 +21,17 @@ func main() {
 	router.HandleFunc("/assignment/{id}", wyoassign.GetAssignment).Methods("GET")
 	router.HandleFunc("/assignment/{id}", wyoassign.DeleteAssignment).Methods("DELETE")		
 	router.HandleFunc("/assignment", wyoassign.CreateAssignment).Methods("POST")	
-	// router.HandleFunc("/assignments/{id}", wyoassign.UpdateAssignment).Methods("PUT")
+	router.HandleFunc("/assignments/{id}", wyoassign.UpdateAssignment).Methods("PUT")
 
-	http.Handle("/", router)
+	//class data struct endpoints + homepage change
+	router.HandleFunc("/", wyoassign.HandleTheHomePage).Methods("GET")
+	router.HandleFunc("/classes", wyoassign.GetClasses).Methods("GET")
+	router.HandleFunc("/class/{id}", wyoassign.GetClass).Methods("GET")
+	router.HandleFunc("/class/{id}", wyoassign.DeleteClass).Methods("DELETE")
+	router.HandleFunc("/class", wyoassign.CreateClass).Methods("POST")
+
+	//http.Handle("/", router)
 
 	//start and listen to requests
 	http.ListenAndServe(":8080", router)
-
 }
--- materials/lab/4/wyoassign/wyoassign.go
+++ materials/lab/4/wyoassign/wyoassign.go
@@ -14,6 +14,11 @@ type Response struct{
 	Assignments []Assignment `json:"assignments"`
 }
 
+//added response struct
+type MyResponse struct{
+	Classes []Class `json:"classes"`
+}
+
 type Assignment struct {
 	Id string `json:"id"`
 	Title string `json:"title`
@@ -21,8 +26,20 @@ type Assignment struct {
 	Points int `json:"points"`
 }
 
+//added class struct
+type Class struct{
+	Id string `json:"id"`
+	CourseNumber int `json:"coursenumber`
+	Name string `json:"name"`
+	Professor string `json:"professor"`
+	Department string `json:"department"`
+}
+
 var Assignments []Assignment
 const Valkey string = "FooKey"
+
+var Classes []Class
+const Valkey2 string = "BarKey"
 
 func InitAssignments(){
 	var assignmnet Assignment
@@ -33,10 +50,26 @@ func InitAssignments(){
 	Assignments = append(Assignments, assignmnet)
 }
 
+func InitClasses(){
+	var class Class
+	class.Id = "Cyber1"
+	class.CourseNumber = 1100
+	class.Department = "COSC"
+	class.Name = "Topics in Cybersecurity"
+	class.Professor = "Mike Borowzek"		//spelled that wrong haha
+	Classes = append(Classes, class)
+}
+
 func APISTATUS(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Entering %s end point", r.URL.Path)
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "API is up and running")
+}
+
+func HandleTheHomePage(w http.ResponseWriter, r *http.Request){
+	log.Printf("Entering %s end point", r.URL.Path)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "You Have Made it to the HomePage BabyGirl")
 }
 
 
@@ -55,7 +88,22 @@ func GetAssignments(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//TODO 
+	w.Write(jsonResponse)
+}
+
+//added get classes function
+func GetClasses(w http.ResponseWriter, r *http.Request){
+	log.Printf("Entering %s end point", r.URL.Path)
+	var response MyResponse
+	response.Classes = Classes
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+		jsonResponse, err := json.Marshal(response)
+	if err!=nil{
+		return
+	}
 	w.Write(jsonResponse)
 }
 
@@ -65,14 +113,52 @@ func GetAssignment(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	params := mux.Vars(r)
 
+	response := make(map[string]string)
+	response["status"] = "No assignments associated with this endpoint"
+
 	for _, assignment := range Assignments {
 		if assignment.Id == params["id"]{
+			response["status"] = "Worked"
 			json.NewEncoder(w).Encode(assignment)
 			break
 		}
 	}
+
 	//TODO : Provide a response if there is no such assignment
-	//w.Write(jsonResponse)
+	if(response["status"] != "Worked"){
+		jsonResponse, err := json.Marshal(response)
+		if err != nil {
+			return 
+		}
+		w.Write(jsonResponse)
+	}
+}
+
+//added get class function. Returns one specific class based off course number
+func GetClass(w http.ResponseWriter, r *http.Request){
+	log.Printf("Entering %s end point", r.URL.Path)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	params := mux.Vars(r)
+
+	response := make(map[string]string)
+	response["status"] = "No classes associated with this endpoint"
+
+	for _, class := range Classes {
+		if class.Id == params["id"]{
+			response["status"] = "Worked"
+			json.NewEncoder(w).Encode(class)
+			break
+		}
+	}
+
+	if(response["status"] != "Worked"){
+		jsonResponse, err := json.Marshal(response)
+		if err != nil {
+			return 
+		}
+		w.Write(jsonResponse)
+	}
 }
 
 func DeleteAssignment(w http.ResponseWriter, r *http.Request) {
@@ -99,13 +185,72 @@ func DeleteAssignment(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonResponse)
 }
 
+//added delete class function
+func DeleteClass(w http.ResponseWriter, r *http.Request){
+	log.Printf("Entering %s DELETE end point", r.URL.Path)
+	w.Header().Set("Content-Type", "application/txt")
+	w.WriteHeader(http.StatusOK)
+	params := mux.Vars(r)
+	
+	response := make(map[string]string)
+
+	response["status"] = "No Such coursenumber to Delete"
+	for index, class := range Classes {
+		if class.Id == params["id"]{
+			Classes = append(Classes[:index], Classes[index+1:]...)
+			response["status"] = "Success"
+			break
+		}
+	}
+
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		return
+	}
+	w.Write(jsonResponse)
+}
+
 func UpdateAssignment(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Entering %s end point", r.URL.Path)
 	w.Header().Set("Content-Type", "application/json")
+
+	messageResponse := make(map[string]string)
+	messageResponse["status"] = "Failed"
 	
 	var response Response
 	response.Assignments = Assignments
+	
+	for index, assignment := range response.Assignments {
+		if assignment.Id == r.FormValue("id") {
+			messageResponse["status"] = "Worked"	//signal that it worked
 
+			Assignments = append(Assignments[:index], Assignments[index+1:]...)
+			var newPost Assignment
+			newPost.Id =  r.FormValue("id")
+			newPost.Title =  r.FormValue("title")
+			newPost.Description =  r.FormValue("desc")
+			newPost.Points, _ =  strconv.Atoi(r.FormValue("points"))
+			Assignments = append(Assignments, newPost)
+
+			w.WriteHeader(http.StatusCreated)
+		}
+	}
+
+	//didn't find assignment to udpate
+	if(messageResponse["status"] != "Worked"){
+		jsonResponse, err := json.Marshal(response)
+		if err != nil {
+			return 
+		}
+		w.Write(jsonResponse)
+	}else{
+		//found assignment to change
+		jsonResponse, err := json.Marshal(response)
+		if err != nil {
+			return 
+		}
+		w.Write(jsonResponse)
+	}
 
 
 }
@@ -126,4 +271,21 @@ func CreateAssignment(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusNotFound)
 
+}
+
+//added create class function
+func CreateClass(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	var class Class
+	r.ParseForm()
+	if(r.FormValue("id") != ""){
+		class.Id = r.FormValue("id")
+		class.Name = r.FormValue("name")
+		class.CourseNumber, _ = strconv.Atoi(r.FormValue("coursenumber"))
+		class.Department = r.FormValue("department")
+		class.Professor = r.FormValue("professor")
+		Classes = append(Classes, class)
+		w.WriteHeader(http.StatusCreated)
+	}
+	w.WriteHeader(http.StatusNotFound)
 }
